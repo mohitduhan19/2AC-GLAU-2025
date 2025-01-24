ad63d606b6b82e2fcb86ff9eea7e809954fcab0d
Mohit Duhan <mohitkumar72956@gmail.com>
2025-01-24 21:17:10 +0530

    String_Ques

---
+++ Lec4_PCPH/Find_The_Index_Of_First_Occurence.java
@@ -0,0 +1,19 @@
+package AC.Lec4_PCPH;
+
+public class Find_The_Index_Of_First_Occurence {
+    public int strStr(String haystack, String needle) {
+        int l1 = haystack.length(), l2 = needle.length();
+        if (l1 < l2) {
+            return -1;
+        } else if (l2 == 0) {
+            return 0;
+        }
+        int threshold = l1 - l2;
+        for (int i = 0; i <= threshold; ++i) {
+            if (haystack.substring(i,i+l2).equals(needle)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
