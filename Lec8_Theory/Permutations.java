477d9cf87d35d54b6b2c5723754b4f22b0cdbf7b
Mohit Duhan <mohitkumar72956@gmail.com>
2025-02-07 13:00:30 +0530

    Recursion

---
+++ Lec8_Theory/Permutations.java
@@ -0,0 +1,27 @@
+package AC.Lec8_Theory;
+import java.util.*;
+public class Permutations {
+    public void  help(List<List<Integer>> ans , List<Integer> curr , int[] nums , boolean[] vis){
+
+        if(curr.size() == nums.length){
+            ans.add(new ArrayList<>(curr));
+            return;
+        }
+        for(int i = 0;i < nums.length;i++){
+            if(vis[i] == true)continue;
+            vis[i] = true;
+            curr.add(nums[i]);
+            help(ans , curr , nums , vis);
+            curr.remove(curr.size() - 1);
+            vis[i] = false;
+        }
+    }
+
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> ans = new ArrayList<>();
+        List<Integer> curr = new ArrayList<>();
+        boolean[] vis= new boolean[nums.length];
+        help(ans , curr , nums , vis);
+        return ans;
+    }
+}
