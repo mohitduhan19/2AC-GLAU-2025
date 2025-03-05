acfd038e35085e5bcde4858b47d072e18e8c9e89
Mohit Duhan <mohitkumar72956@gmail.com>
2025-03-05 14:59:09 +0530

    Stack

---
+++ Lec11_Theory/Valid_Paranthesis.java
@@ -0,0 +1,23 @@
+package AC.Lec11_Theory;
+import java.util.*;
+
+public class Valid_Paranthesis {
+    public boolean isValid(String s) {
+        Stack<Character> st = new Stack<>();
+
+        for(int i = 0;i < s.length();i++){
+            char ch = s.charAt(i);
+
+            if(ch == '(' || ch == '[' || ch == '{')st.push(ch);
+            else{
+                if(st.size() == 0)return false;
+                char c = st.peek();
+
+                if( (ch == ')' && c == '(' ) || ( ch == ']' && c == '[' ) || (ch == '}' && c == '{') )st.pop();
+                else return false;
+            }
+        }
+        if(st.size() == 0) return true;
+        return false;
+    }
+}
