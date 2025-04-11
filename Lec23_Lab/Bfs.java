bfa906c1ffa59a2acd776e211292cc48e38e2eea
Mohit Duhan <mohitkumar72956@gmail.com>
2025-04-11 15:49:27 +0530

    graph

---
+++ Lec23_Lab/Bfs.java
@@ -0,0 +1,24 @@
+package AC.Lec23_Lab;
+import java.util.*;
+public class Bfs {
+    public ArrayList<Integer> bfsOfGraph(int V, ArrayList<ArrayList<Integer>> adj){
+        ArrayList<Integer> ls = new ArrayList<>();
+        boolean[] vis = new boolean[V];
+        Queue<Integer> q = new ArrayDeque<>();
+        q.add(0);
+        vis[0] = true;
+
+        while(!q.isEmpty()){
+            Integer node = q.poll();
+            ls.add(node);
+
+            for(Integer i : adj.get(node)){
+                if(vis[i] == false){
+                    vis[i] = true;
+                    q.add(i);
+                }
+            }
+        }
+        return ls;
+    }
+}
---
+++ Lec23_Lab/Dfs.java
@@ -0,0 +1,22 @@
+package AC.Lec23_Lab;
+import java.util.*;
+
+public class Dfs {
+    public void dfs(int node ,boolean[] vis , ArrayList<ArrayList<Integer>> adj , ArrayList<Integer> lis){
+        vis[node] = true;
+        lis.add(node);
+
+        for(Integer i : adj.get(node)){
+            if(vis[i] == false){
+                dfs(i , vis , adj , lis);
+            }
+        }
+    }
+    public ArrayList<Integer> dfsOfGraph(ArrayList<ArrayList<Integer>> adj) {
+        boolean[] vis = new boolean[adj.size()+1];
+        vis[0] = true;
+        ArrayList<Integer> lis = new ArrayList<>();
+        dfs(0 , vis, adj , lis);
+        return lis;
+    }
+}
